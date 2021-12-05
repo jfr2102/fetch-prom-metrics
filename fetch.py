2f6bb029b511b342346aa8cacce1468f35bc3a39
jfr2102 <johannes@friedlein.eu>
2021-12-05 20:29:55 +0100

    update metric path

--- fetch.py
+++ fetch.py
@@ -2,24 +2,27 @@ from parides.prom_conv import from_prom_to_df
 from datetime import datetime as dt, datetime
 from datetime import timedelta
 import os
+import sys
 from matplotlib import pyplot
 
 # List of queries to fetch
-from querylist import *
-#from myquerylist import *
+#from querylist import *
+from myquerylist import *
 
 # Config
-from config import *
-#from myconfig import *
+#from config import *
+from myconfig import *
 try:
     os.mkdir("prom-metrics")
     print("created prom-metrics directory")
 except FileExistsError:
     print("prom-metrics directory already exists")
 now = dt.now()
-path = (now.strftime("%Y-%m-%d-%H_%M"))
+if len(sys.argv) == 2:
+    path = sys.argv[1]
+else:
+    path = (now.strftime("%Y-%m-%d-%H_%M"))
 os.mkdir("prom-metrics/"+path)
-
 for q in queries:
     df = from_prom_to_df(
         resolution    = resolution,
