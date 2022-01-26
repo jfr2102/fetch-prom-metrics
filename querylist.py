05a2738557686d3902446dc210897239c854b90b
jfr2102 <johannes@friedlein.eu>
2022-01-26 16:24:58 +0100

    example query

--- querylist.py
+++ querylist.py
@@ -1,3 +1,12 @@
 queries = [("QUERYNAME", "PROM_QUERY"),
            ("QUERYNAME", "PROM_QUERY")
 ]
+
+# Example:
+# queries = [ ("storm_topology_Parser_Executed_count_KAFKA", "storm_topology_Parser_Executed_count{component_id=\"bolt\", topology_id=~\".*Kafka.*\"}")
+# ,   ("storm_topology_WindowBolt_Executed_count_KAFKA", "storm_topology_WindowBolt_Executed_count{topology_id=~\".*Kafka.*\"}")
+# ,   ("storm_topology_Windowbolt_Throughput_sum", "sum(rate(storm_topology_WindowBolt_Executed_count{topology_id=~\".*Kafka.*\"}[20s]))")
+# ,   ("storm_topology_Windowbolt_Throughput", "rate(storm_topology_WindowBolt_Executed_count{topology_id=~\".*Kafka.*\"}[20s])")
+# ,   ("node_cpu_seconds_total_1m", "avg without (mode,cpu) (1 - rate(node_cpu_seconds_total{mode=\"idle\"}[1m]))")
+# ]
+                                                                                                                    
